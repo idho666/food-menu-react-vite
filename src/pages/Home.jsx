43918ff79714ff8acaa042da7e151e20a2408e53
hafidh bm <hafidh.mauidho@iconpln.co.id>
2024-08-08 13:27:16 +0700

    feat : add component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Hero from "../componen/Hero";
+import Service from "../componen/Service";
+import About from "../componen/About";
 
 function Home() {
   return (
     <div>
       <Hero />
+      <Service />
+      <About />
     </div>
   );
 }
